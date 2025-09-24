2b0613f32b9a89bcb8c704c76f8d81a70c5cb680
Meshplex <tercan.piricek1@gmail.com>
2025-09-24 10:01:06 +0200

    removed unused code

--- led.py
+++ led.py
@@ -2,25 +2,6 @@ import RPi.GPIO as GPIO
 
 # LED / GPIO Hilfsfunktionen (minimalistisch)
 
-def usable(pin):
-    """Testet ob Pin HIGH wird (Blocking bis erster Read)"""
-    try:
-        # Set up the GPIO pin
-        GPIO.setmode(GPIO.BCM)
-        # Set the pin as an input with an optional pull-up or pull-down resistor
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    
-        while True:
-            # Read the state of the pin
-            if GPIO.input(pin):
-                return True
-            else:
-                return False
-    except KeyboardInterrupt:
-        pass
-    finally:
-        GPIO.cleanup(pin)
-
 def setup_led(pin):
     """Richtet Pin als OUTPUT ein"""
     GPIO.setmode(GPIO.BCM)
